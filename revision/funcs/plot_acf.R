236c971ab8757893576eefce75088d242a96696d
oscarfergue98 <98018048+oscarfergue98@users.noreply.github.com>
2023-12-28 16:16:44 +0100

    Remove certain revision files

--- revision/funcs/plot_acf.R
+++
@@ -1,8 +0,0 @@
-plot.acf <- function(my_country, model_resids){ 
-  
-  data.residuals %>% 
-    filter(country == my_country) %>% 
-    pull(model_resids) %>% 
-    acf(main = str_to_title(my_country))
-  
-}
